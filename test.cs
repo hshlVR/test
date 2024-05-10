5b06851f21f5bf1ae7bba4eeb892a7476dbc211e
hshlVR <147735381+hshlVR@users.noreply.github.com>
2024-05-10 11:15:45 +0200

    Initial commit

---
+++ test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void Ein_Test()
+        {
+            Program program = new Program();
+            Assert.That(program.HelloWorld() == "Hello world!");
+        }
+    }
+}
